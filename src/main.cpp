e77b98a9d66678fa0391c3bdc6c619d1dfee351c
Miguel Peláez <kernelfreeze@outlook.com>
2018-07-12 14:54:32 -0500

    Allow stream forever

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ void writeConfig() {
     config << "[stream]" << endl
            << "version=1" << endl
            << "token=MIXERTOKEN" << endl
+           << "forever=false" << endl
            << "url=rtmp://ingest-par.mixer.com:1935/beam"
            << " ; https://watchbeam.zendesk.com/hc/en-us/articles/"
               "209659883-How-to-change-your-Ingest-Server"
@@ -100,78 +101,80 @@ int main(int argc, char const *argv[]) {
     FD_ZERO(&sockset);
     FD_SET(RTMP_Socket(rtmp), &sockset);
 
-    for (auto &file : fs::directory_iterator(VIDEO_PATH)) {
-        auto input_file = file.path().c_str();
+    while (reader.GetBoolean("stream", "forever", false)) {
+        for (auto &file : fs::directory_iterator(VIDEO_PATH)) {
+            auto input_file = file.path().c_str();
 
-        LOG_INFO << "Now playing " << file.path();
+            LOG_INFO << "Now playing " << file.path();
 
-        // Load FLV file
-        auto flvin = flv_open(input_file);
+            // Load FLV file
+            auto flvin = flv_open(input_file);
 
-        if (!flvin) {
-            LOG_CRIT << "Unable to open " << input_file;
-            continue;
-        }
-
-        flv_header header;
-        int res = flv_read_header(flvin, &header);
-        if (res == FLV_ERROR_NO_FLV || res == FLV_ERROR_EOF) {
-            LOG_CRIT << "File is not an FLV video";
-            flv_close(flvin);
-            continue;
-        }
-
-        auto buffer = new char[BUFFER_SIZE];
-        flv_tag tag;
-
-        auto started = now();
-
-        // Decode FLV data
-        while (flv_read_tag(flvin, &tag) != FLV_ERROR_EOF) {
-            // copy tag header
-            flv_copy_tag(buffer, &tag, BUFFER_SIZE);
-
-            // copy tag body
-            auto data_size =
-                flv_read_tag_body(flvin, buffer + FLV_TAG_SIZE, BUFFER_SIZE - FLV_TAG_SIZE);
-
-            // copy previous tag size
-            uint32 pts;
-            flv_read_prev_tag_size(flvin, &pts);
-            flv_copy_prev_tag_size(buffer + FLV_TAG_SIZE + flv_tag_get_body_length(tag), pts,
-                BUFFER_SIZE - (FLV_TAG_SIZE + flv_tag_get_body_length(tag)));
-
-            // write the packet
-            int size = FLV_TAG_SIZE + data_size + sizeof(uint32);
-
-            // Wait before sending new frames
-            auto current = now() - started;
-            auto frame = uint24_be_to_uint32(tag.timestamp);
-
-            if (frame > current) {
-                this_thread::sleep_for(milliseconds(frame - current));
+            if (!flvin) {
+                LOG_CRIT << "Unable to open " << input_file;
+                continue;
             }
 
-            if (RTMP_Write(rtmp, buffer, size) <= 0) {
-                LOG_CRIT << "Unable to write bytes to server";
-                break;
+            flv_header header;
+            int res = flv_read_header(flvin, &header);
+            if (res == FLV_ERROR_NO_FLV || res == FLV_ERROR_EOF) {
+                LOG_CRIT << "File is not an FLV video";
+                flv_close(flvin);
+                continue;
             }
 
-            // Handle RTMP ping and other stuff
-            int result = select(RTMP_Socket(rtmp) + 1, &sockset, NULL, NULL, &timeout);
+            auto buffer = new char[BUFFER_SIZE];
+            flv_tag tag;
 
-            if (result == 1 && FD_ISSET(RTMP_Socket(rtmp), &sockset)) {
-                RTMP_ReadPacket(rtmp, &rtmpPacket);
+            auto started = now();
 
-                if (!RTMPPacket_IsReady(&rtmpPacket)) {
-                    RTMP_ClientPacket(rtmp, &rtmpPacket);
-                    RTMPPacket_Free(&rtmpPacket);
+            // Decode FLV data
+            while (flv_read_tag(flvin, &tag) != FLV_ERROR_EOF) {
+                // copy tag header
+                flv_copy_tag(buffer, &tag, BUFFER_SIZE);
+
+                // copy tag body
+                auto data_size =
+                    flv_read_tag_body(flvin, buffer + FLV_TAG_SIZE, BUFFER_SIZE - FLV_TAG_SIZE);
+
+                // copy previous tag size
+                uint32 pts;
+                flv_read_prev_tag_size(flvin, &pts);
+                flv_copy_prev_tag_size(buffer + FLV_TAG_SIZE + flv_tag_get_body_length(tag), pts,
+                    BUFFER_SIZE - (FLV_TAG_SIZE + flv_tag_get_body_length(tag)));
+
+                // write the packet
+                int size = FLV_TAG_SIZE + data_size + sizeof(uint32);
+
+                // Wait before sending new frames
+                auto current = now() - started;
+                auto frame = uint24_be_to_uint32(tag.timestamp);
+
+                if (frame > current) {
+                    this_thread::sleep_for(milliseconds(frame - current));
+                }
+
+                if (RTMP_Write(rtmp, buffer, size) <= 0) {
+                    LOG_CRIT << "Unable to write bytes to server";
+                    break;
+                }
+
+                // Handle RTMP ping and other stuff
+                int result = select(RTMP_Socket(rtmp) + 1, &sockset, NULL, NULL, &timeout);
+
+                if (result == 1 && FD_ISSET(RTMP_Socket(rtmp), &sockset)) {
+                    RTMP_ReadPacket(rtmp, &rtmpPacket);
+
+                    if (!RTMPPacket_IsReady(&rtmpPacket)) {
+                        RTMP_ClientPacket(rtmp, &rtmpPacket);
+                        RTMPPacket_Free(&rtmpPacket);
+                    }
                 }
             }
-        }
 
-        flv_close(flvin);
-        delete[] buffer;
+            flv_close(flvin);
+            delete[] buffer;
+        }
     }
 
     LOG_INFO << "Done! All files were broadcasted.";
