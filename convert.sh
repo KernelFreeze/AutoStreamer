e7614395d66237d8dbc4e07c1f87dd84c75707c4
Miguel Peláez <kernelfreeze@outlook.com>
2018-07-12 20:04:46 +0000

    Ignore extension

--- convert.sh
+++ convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p videos
-for f in videos/*.mp4; do
+shopt -s extglob
+
+for f in videos/!(*.flv); do
   ffmpeg -i $f -c:v libx264 -ar 22050 -crf 28 videos/$(basename ${f%.*}.flv)
   rm $f
 done
