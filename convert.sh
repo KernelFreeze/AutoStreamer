ee00e50b63b8b6bd5ff01654617483d31bd28fb3
KernelFreeze <kernelfreeze@outlook.com>
2018-07-23 15:03:42 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -3,8 +3,7 @@ mkdir -p videos
 shopt -s extglob
 
 for f in videos/!(*.flv); do
-  ffmpeg -i $f -c:v libx264 -s 720x480 -ar 22050 -crf 28 
-videos/$(basename ${f%.*}.flv)
+  ffmpeg -i $f -c:v libx264 -s 720x480 -ar 22050 -crf 28 videos/$(basename ${f%.*}.flv)
   rm $f
 done
 
